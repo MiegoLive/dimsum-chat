5ac88e853771823c54e09d1aa6657412b72d9d17
Terry <TerryYoung518@hotmail.com>
2024-04-04 18:37:28 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export { WebSocketManager, getBfaceURL, getWebSocketURL} from './websocket';
+export { WebSocketManager, getBfaceURL, getWebSocketURL, onMessage} from './websocket';
 export { Parser } from './parser';
 export type { commentParseOptions, abstractLevelOptions } from './parser';
